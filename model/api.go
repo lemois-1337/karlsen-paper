aff454ad9f6905f175c785a9a61bb91f488bf85a
Lemois 1337 <lemois1337@protonmail.com>
2023-12-18 13:38:06 +0100

    Show extended public key QR:
    * Upstream PR: https://github.com/svarogg/kaspaper/pull/10
    
      Extended public keys QR code is needed for upcoming mobile
      wallet. We've introduced some small changes to original PR in
      Kaspaper for better viuals and readability.

--- model/api.go
+++ model/api.go
@@ -8,4 +8,6 @@ type PaperWallet interface {
 	Mnemonic() *MnemonicString
 	Address(index int) (string, error)
 	AddressQR(index int) ([]byte, error)
+	KPubKey() (string)
+	KPubKeyQR() ([]byte, error)
 }
